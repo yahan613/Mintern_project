102b6aeddb18fb4ebc6b6c3093586be96c19c2eb
yahan0613 <yahan930718@gmail.com>
2023-10-21 22:56:14 +0800

    <填寫版本資訊>

---
+++ app.js
@@ -0,0 +1,43 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+
+
+function PageTransitions(){
+    document.addEventListener('DOMContentLoaded', function() {
+        const sectBtn = document.querySelectorAll('.control');
+    
+        for (let i = 0; i < sectBtn.length; i++) {
+            sectBtn[i].addEventListener('click', function() {
+                let currentBtn = document.querySelector(".active-btn");
+                currentBtn.className = currentBtn.className.replace('active-btn', '');
+                this.className += ' active-btn';
+            });
+        }
+    });
+
+    //Section Active
+    allSections.addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+        if(id){
+            //remove selected from the other btns
+            sectBtns.forEach((btn) => {
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            //hide other sections
+            sections.forEach((section) => {
+                section.classList.remove('active')
+            })
+            const element = document.getElementById(id);
+            element.classList.add('active')
+        }
+    })
+}
+
+// 執行 `PageTransitions`
+
+
+PageTransitions();
