791192cd07f39acd5c00724c738c729073be440e
Claudia Wang <claudia350.tw@gmail.com>
2023-10-22 14:32:59 +0800

    10/22_modified

--- app.js
+++ app.js
@@ -41,3 +41,19 @@ function PageTransitions(){
 
 
 PageTransitions();
+
+PageTransitions();
+
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
