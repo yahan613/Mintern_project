920dc5fed9879a7f73e2efdebf3551d9f25b30ae
yahan0613 <yahan930718@gmail.com>
2023-12-07 02:32:16 +0800

    loading

--- app.js
+++ app.js
@@ -105,28 +105,59 @@ PageTransitions();
     });
 })();
 
-// 使用 matchMedia 檢查媒體查詢是否符合
-const mediaQuery = window.matchMedia("(max-width: 600px)");
+// 創建一個 MediaQueryList 對象
+/*const mediaQuery = window.matchMedia("(max-width: 600px)");
 
-// 定義處理媒體查詢生效的函數
-const handleMediaQuery = (event) => {
+// 定義處理媒體查詢變化的函數
+const handleMediaQueryChange = (event) => {
     const loader = document.querySelector(".rwd-loading");
 
-    // 如果媒體查詢生效
     if (event.matches) {
         // 在這裡添加 .rwd-loading--visible 的 class
         loader.classList.add("rwd-loading--visible");
-    } 
+    } else {
+        // 如果媒體查詢不再生效，你可以執行其他動作，例如移除 .rwd-loading--visible 的 class
+        loader.classList.remove("rwd-loading--visible");
+    }
+
+    // 在這裡執行其他相應的代碼
     setTimeout(() => {
         loader.classList.remove("rwd-loading--visible");
-    }, 5000);
+    }, 3000);
 };
 
-// 添加媒體查詢的事件監聽器
-mediaQuery.addEventListener("change", handleMediaQuery);
+// 添加媒體查詢變化的事件監聽器
+mediaQuery.addEventListener("change", handleMediaQueryChange);
 
 // 初始狀態下檢查媒體查詢
-handleMediaQuery(mediaQuery);
+handleMediaQueryChange(mediaQuery);*/
 
+let previousWidth = window.innerWidth;
 
+const handleResize = () => {
+    const currentWidth = window.innerWidth;
+    console.log(`當前網頁寬度為: ${currentWidth}px`);
+    const loader = document.querySelector(".rwd-loading");
 
+    // 在這裡可以比較 currentWidth 和 previousWidth，執行相應的操作
+    if (currentWidth != previousWidth) {
+        console.log("寬度改變了！");
+        loader.classList.add("rwd-loading--visible");
+    } else {
+        loader.classList.remove("rwd-loading--visible");
+    }
+    setTimeout(() => {
+        /*loader.classList.remove("rwd-loading--visible");*/
+        loader.classList.add("rwd-loading--hidden");
+        console.log("移除");
+    }, 3000);
+    // 更新 previousWidth
+    previousWidth = currentWidth;
+    loader.classList.remove("rwd-loading--hidden");
+};
+
+// 添加螢幕寬度變化的事件監聽器
+window.addEventListener("resize", handleResize);
+
+// 初始狀態下檢查螢幕寬度
+handleResize();
